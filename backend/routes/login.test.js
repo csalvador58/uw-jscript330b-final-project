fe86a633b79c81f16fa826194462fcddf7670871
Chris Salvador <csalvador58@gmail.com>
2023-05-28 00:00:04 -0700

    setup tests

--- backend/routes/login.test.js
+++ backend/routes/login.test.js
@@ -11,21 +11,37 @@ describe('/login', () => {
 
   afterEach(testUtils.clearDB);
 
+  const adminUser = {};
+  const vendorUser = {};
+  const verifierUser = {};
+
   describe('before user is created in the system', () => {
     describe('POST /', () => {});
     describe('POST /updateUser', () => {});
     describe('POST /updatePassword', () => {});
+    describe('POST /addUserData', () => {});
     describe('GET /user', () => {});
     describe('GET /userData', () => {});
-    describe('PUT /user', () => {});
-    describe('PUT /userData', () => {});
+    describe('PUT /createUser', () => {});
+    describe('PUT /addUserData', () => {});
   });
   describe('after user is created in the system', () => {
     describe('POST /', () => {});
-    describe('', () => {});
-    describe('', () => {});
-    describe('', () => {});
-    describe('', () => {});
-    describe('', () => {});
   });
+  describe.each([adminUser, vendorUser, verifierUser])('User %#', (user) => {
+    beforeEach(async () => {
+      await request(server).post('/login/user').send(adminUser);
+      await request(server).post('/login/user').send(vendorUser);
+      await request(server).post('/login/user').send(verifierUser);
+    });
+    describe('POST /', () => {});
+  });
+  describe('after admin, vendor, and verifier users are logged in', () => {
+    let adminToken;
+    let vendorToken;
+    let verifierToken;
+  });
+
+  describe('', () => {});
+  describe('', () => {});
 });
