7917ccf244295afd4570c1553d3c8c1c7bd171ad
Chris Salvador <csalvador58@gmail.com>
2023-06-13 16:14:16 -0700

    update index for deploy

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -15,6 +15,18 @@ router.use('/admin', require('../routes/admin'));
 router.use('/vendor', require('./vendor'));
 router.use('/verifier', require('./verifier'));
 
+// For deploy test only
+router.get('/', (req, res, next) => {
+  res.send(`
+    <html>
+      <body>
+        <h1> Server deployed! </h1>
+      </body>
+    </html>
+  `);
+});
+
+
 router.use((req, res, next) => {
   res.status(404).json({ error: 'Not found' });
 });
